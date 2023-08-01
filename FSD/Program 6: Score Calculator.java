8223dffd3c8e7e96b2dd615efdda338837db5c67
randomplayer2002 <amith.hathwar02@gmail.com>
2023-08-01 10:27:34 +0530

    Done

---
+++ FSD/Program 6: Score Calculator.java
@@ -0,0 +1,41 @@
+// WARNING: Do not edit the code outside the region BEGIN and END mentioned below. If you do so, the test cases may not pass.
+ 
+// Read the variable from STDIN
+process.stdin.on('data', function(chunk) {
+    let lines = chunk.toString().split('\n');
+    let score1 = parseInt(lines[0]);
+    let score2 = parseInt(lines[1]);
+   
+    // BEGIN: Write all your code below this line
+   
+    /* DONE 1: Wrote a function named 'loading' to log following message after 1 second on to console
+        Loading score...
+    */
+    /**
+     * Function to print the message 'Loading score...' on the console after 1 second
+     */
+    const loading = () => {
+        setTimeout(() =>{
+            console.log('Loading score...');
+        }, 1000);
+    }
+     
+    // DONE 2: Wrote a function named 'displayScore' to log score after 2 seconds on to console
+    /**
+     * Function to display score of Arsenal and Liverpool after 2 seconds
+     * @param {Number} score1 - score secured by Arsenal
+     * @param {Number} score2 - score secured by Liverpool
+     * Note that the score must be displayed in the format given in the problem description.
+     */
+    const displayScore = (score1, score2) => {
+        setTimeout(() =>{
+            console.log(`Arsenal ${score1}-${score2} Liverpool`);
+        }, 2000);
+    }
+   
+    // END: Write all your code above this line
+    
+    loading();
+    displayScore(score1, score2);
+});
+
