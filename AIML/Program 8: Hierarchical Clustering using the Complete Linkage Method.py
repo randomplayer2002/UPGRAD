2ec3136e74b9ad9c4fd357163decac61f76cf26f
randomplayer2002 <amith.hathwar02@gmail.com>
2023-08-05 21:53:48 +0530

    Done

--- AIML/Program 8: Hierarchical Clustering using the Complete Linkage Method.py
+++ AIML/Program 8: Hierarchical Clustering using the Complete Linkage Method.py
@@ -1,23 +1,14 @@
 import numpy as np
-from scipy.cluster.hierarchy import linkage, fcluster
-
-def hierarchical_clustering(X, threshold, method='complete'):
-    # Calculate the distance matrix using the complete linkage method
-    distance_matrix = linkage(X, method=method)
-    
-    # Perform hierarchical clustering and get the cluster labels
-    cluster_labels = fcluster(distance_matrix, threshold, criterion='distance')
-    
-    return cluster_labels
+from sklearn.cluster import AgglomerativeClustering
+import matplotlib.pyplot as plt
 
 # Input data
 X = np.array([[1, 2], [2, 3], [10, 12], [11, 13], [20, 25], [22, 24]])
 
-# Threshold for forming clusters (you can adjust this value as needed)
-threshold = 5
+# Perform hierarchical clustering using complete linkage
+clustering = AgglomerativeClustering(linkage='complete')
+clustering.fit(X)
 
-# Perform hierarchical clustering using the complete linkage method
-cluster_labels = hierarchical_clustering(X, threshold, method='complete')
-
-# Print the output
-print("Cluster Labels:", cluster_labels.tolist())
+# Get the cluster assignments for each data point
+labels = clustering.labels_
+labels
