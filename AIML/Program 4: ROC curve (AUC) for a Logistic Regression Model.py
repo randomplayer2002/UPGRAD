a50a362ea55e1e3d528c80fea00dadd92fde52f0
randomplayer2002 <amith.hathwar02@gmail.com>
2023-08-05 21:35:55 +0530

    Done

--- AIML/Program 4: ROC curve (AUC) for a Logistic Regression Model.py
+++ AIML/Program 4: ROC curve (AUC) for a Logistic Regression Model.py
@@ -1,16 +1,12 @@
-import numpy as np
-from sklearn.metrics import roc_curve, auc
+from sklearn.metrics import roc_auc_score
+
+y_true = [0, 1, 1, 0, 1]
+y_pred = [0.2, 0.8, 0.6, 0.3, 0.9]
 
 def calculate_auc(y_true, y_pred):
-    fpr, tpr, _ = roc_curve(y_true, y_pred)
-    auc_score = auc(fpr, tpr)
-    return auc_score
+    auc = roc_auc_score(y_true, y_pred)
+    return auc
 
-# Given true class labels and predicted probabilities
-y_true = np.array([0, 1, 1, 0, 1])
-y_pred = np.array([0.2, 0.8, 0.6, 0.3, 0.9])
-
-# Calculate AUC
 auc_score = calculate_auc(y_true, y_pred)
 
-print(f"AUC: {auc_score:.3f}")
+print("Area Under the ROC Curve (AUC): {:.2f}".format(auc_score))
