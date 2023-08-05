6201a9652f965b10a398d51918ec8b87a8f63488
randomplayer2002 <amith.hathwar02@gmail.com>
2023-08-05 21:17:01 +0530

    Done

--- AIML/Program 1: Stochastic Gradient Descent (SGD).py
+++ AIML/Program 1: Stochastic Gradient Descent (SGD).py
@@ -1,43 +1,29 @@
-import random
-import numpy as np
-
-def linear_regression_sgd(X, y, learning_rate=0.01, batch_size=1, num_iterations=1000, regularization_term=0.0):
-    n_samples, n_features = len(X), len(X[0])
-
-    # Initialize coefficients with zeros
-    coefficients = [0.0 for _ in range(n_features)]
-
-    for _ in range(num_iterations):
-        # Shuffle the data and create mini-batches
-        data = list(zip(X, y))
-        random.shuffle(data)
-        mini_batches = [data[i:i+batch_size] for i in range(0, n_samples, batch_size)]
-
-        for mini_batch in mini_batches:
-            X_batch, y_batch = zip(*mini_batch)
-            X_batch, y_batch = np.array(X_batch), np.array(y_batch)
-
-            # Compute the gradient of the loss function with respect to the coefficients
-            y_pred = X_batch.dot(coefficients)
-            error = y_pred - y_batch
-            gradient = X_batch.T.dot(error) / batch_size
-
-            # Add regularization to the gradient (excluding the intercept term)
-            gradient[1:] += (regularization_term * coefficients[1:]) / batch_size
-
-            # Update the coefficients using the learning rate and gradient
-            coefficients -= learning_rate * gradient
-    return coefficients.tolist()
-# Input data
 X = [[1, 1], [1, 2], [1, 3], [1, 4]]
 y = [3, 4, 5, 6]
 learning_rate = 0.01
 batch_size = 2
 num_iterations = 1000
 regularization_term = 0.1
+random_state=42
+from sklearn.linear_model import SGDRegressor
+from sklearn.preprocessing import StandardScaler
 
-# Train the linear regression model using stochastic gradient descent
-coefficients = linear_regression_sgd(X, y, learning_rate, batch_size, num_iterations, regularization_term)
+def train_linear_regression(X, y, learning_rate, batch_size, regularization_term, max_iterations):
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+    model = SGDRegressor(loss='squared_error',learning_rate='constant',eta0=learning_rate,alpha=regularization_term,max_iter=max_iterations,tol=1e-3,random_state=0)
+    n_samples = X_scaled.shape[0]
+    for iteration in range(max_iterations):
+        start_idx = 0
+        while start_idx < n_samples:
+                end_idx = start_idx + batch_size
+                X_batch = X_scaled[start_idx:end_idx]
+                y_batch = y[start_idx:end_idx]
+                model.partial_fit(X_batch, y_batch)
+                start_idx = end_idx
 
-# Print the result
-print("Optimized coefficients:", coefficients)
+    return model
+
+model = train_linear_regression(X,y,learning_rate, batch_size ,regularization_term ,num_iterations)
+
+model.coef_
