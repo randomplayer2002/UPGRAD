d9cf42f3f7ec9ec43e9022e95ad01af149679896
randomplayer2002 <amith.hathwar02@gmail.com>
2023-08-08 15:36:56 +0530

    Done

--- AIML/Program 10: Clustering using the Fuzzy C-Means Algorithm.py
+++ AIML/Program 10: Clustering using the Fuzzy C-Means Algorithm.py
@@ -4,13 +4,15 @@ from sklearn.cluster import KMeans
 
 # Input data
 X = np.array([[1, 2], [2, 3], [10, 12], [11, 13], [20, 25], [22, 24]])
-k = 3
+c = 3
 
 # Perform k-means clustering
-kmeans = KMeans(n_clusters=k, init='k-means++', random_state=0)
+kmeans = KMeans(n_clusters=c, init='k-means++', random_state=0)
 y_kmeans = kmeans.fit_predict(X)
 
 # Extract the cluster centers and labels
 cluster_centers = kmeans.cluster_centers_
 labels = kmeans.labels_
-labels
+
+print('The required Fuzzy-c-Means Clustering:')
+print('For 3-Cluster Centers -->',labels)
