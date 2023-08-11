cd16620a6da401807346be5f63e1f2f2cc76c034
randomplayer2002 <amith.hathwar02@gmail.com>
2023-08-11 07:12:04 +0530

    Done

--- AIML/Program 10: Clustering using the KMeans++ Algorithm.py
+++ AIML/Program 10: Clustering using the KMeans++ Algorithm.py
@@ -4,15 +4,15 @@ from sklearn.cluster import KMeans
 
 # Input data
 X = np.array([[1, 2], [2, 3], [10, 12], [11, 13], [20, 25], [22, 24]])
-c = 3
+k = 3
 
 # Perform k-means clustering
-kmeans = KMeans(n_clusters=c, init='k-means++', random_state=0)
+kmeans = KMeans(n_clusters=k, init='k-means++', random_state=0)
 y_kmeans = kmeans.fit_predict(X)
 
 # Extract the cluster centers and labels
 cluster_centers = kmeans.cluster_centers_
 labels = kmeans.labels_
 
-print('The required Fuzzy-c-Means Clustering:')
+print('The required KMeans++ Clustering:')
 print('For 3-Cluster Centers -->',labels)
